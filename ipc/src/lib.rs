7c01682ed2763169aaf60be8ce27fd96cfae8c3c
Nova <technobaboo@gmail.com>
2023-09-07 12:24:54 -0400

    feat(ipc): display messages

--- ipc/src/lib.rs
+++ ipc/src/lib.rs
@@ -4,6 +4,7 @@ use flexbuffers::FlexbufferSerializer;
 use mint::Vector2;
 use serde::{Deserialize, Serialize};
 use std::{
+	fmt::Display,
 	io::{ErrorKind, Read, Write},
 	vec,
 };
@@ -21,14 +22,38 @@ pub enum Message {
 	MouseAxisDiscrete(Vector2<f32>),
 	Disconnect,
 }
+impl Display for Message {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		f.write_str(&match self {
+			Message::Keymap(k) => format!("Updated keymap with length {}", k.len()),
+			Message::Key { keycode, pressed } => {
+				if *pressed {
+					format!("Pressed key {keycode}")
+				} else {
+					format!("Released key {keycode}")
+				}
+			}
+			Message::MouseMove(delta) => format!("Mouse moved with delta {:?}", *delta),
+			Message::MouseButton { button, pressed } => {
+				if *pressed {
+					format!("Pressed mouse {button}")
+				} else {
+					format!("Released mouse {button}")
+				}
+			}
+			Message::MouseAxisContinuous(a) => format!("Mouse axis continuous {a:?}"),
+			Message::MouseAxisDiscrete(a) => format!("Mouse axis discrete {a:?}"),
+			Message::Disconnect => format!("Disconnect request"),
+		})
+	}
+}
 
 pub fn send_input_ipc(message: Message) {
 	let buf = flexbuffers::to_vec(message).unwrap();
 	let mut stdout = std::io::stdout().lock();
-	stdout
-		.write_all(&(dbg!(buf.len()) as u32).to_be_bytes())
-		.unwrap();
+	stdout.write_all(&(buf.len() as u32).to_be_bytes()).unwrap();
 	stdout.write_all(&buf).unwrap();
+	stdout.flush();
 }
 
 pub async fn receive_input_async_ipc() -> std::io::Result<Message> {
