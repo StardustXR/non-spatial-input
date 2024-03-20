4cc8c8b9a8636cbf5e87717ea1879b28c738e655
Nicola Guerrera <guerrera.nicola@gmail.com>
2024-03-20 23:28:13 +0100

    Update winit

--- manifold/src/input_window.rs
+++ manifold/src/input_window.rs
@@ -4,17 +4,21 @@ use ipc::{send_input_ipc, Message};
 use map_range::MapRange;
 use softbuffer::{Context, Surface};
 use std::{
+	iter::repeat_with,
 	num::NonZeroU32,
 	process::exit,
+	rc::Rc,
 	sync::mpsc::{self, Sender},
 };
 use winit::{
 	dpi::{LogicalPosition, PhysicalPosition, Size},
 	event::{
-		ElementState, Event, KeyboardInput, ModifiersState, MouseButton, VirtualKeyCode,
+		DeviceEvent, ElementState, Event, KeyEvent, Modifiers, MouseButton, MouseScrollDelta,
 		WindowEvent,
 	},
-	event_loop::EventLoop,
+	event_loop::{EventLoop, EventLoopWindowTarget},
+	keyboard::{Key, KeyCode, ModifiersKeyState, NativeKey, PhysicalKey},
+	platform::scancode::PhysicalKeyExtScancode,
 	window::{CursorGrabMode, Window, WindowBuilder},
 };
 
@@ -27,57 +31,99 @@ fn line_dist(p: Vec2, l1: Vec2, l2: Vec2, thickness: f32) -> f32 {
 }
 
 pub struct InputWindow {
-	window: Window,
-	surface: Surface,
-	cursor_position: Option<LogicalPosition<u32>>,
+	window: Rc<Window>,
+	surface: Surface<Rc<Window>, Rc<Window>>,
+	mouse_delta: Option<LogicalPosition<f64>>,
 	grabbed: bool,
-	modifiers: ModifiersState,
+	modifiers: Modifiers,
 	input_loop_tx: Sender<StateChange>,
 }
 impl InputWindow {
 	pub fn new(event_loop: &EventLoop<()>) -> Self {
-		let size = Size::Logical([512, 512].into());
-		let window = WindowBuilder::new()
-			.with_title("Manifold")
-			.with_min_inner_size(size)
-			.with_max_inner_size(size)
-			.with_inner_size(size)
-			.with_resizable(false)
-			.build(event_loop)
-			.unwrap();
+		let size = Size::Logical([128, 128].into());
+		let window = Rc::new(
+			WindowBuilder::new()
+				.with_title("Manifold")
+				.with_min_inner_size(size)
+				.build(&event_loop)
+				.unwrap(),
+		);
 
 		let (input_loop_tx, rx) = mpsc::channel();
 		std::thread::spawn(move || eclipse::input_loop(false, rx));
 
-		let surface = unsafe { Surface::new(&Context::new(&window).unwrap(), &window) }.unwrap();
+		let context = softbuffer::Context::new(window.clone()).unwrap();
+		let surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
 
 		let mut input_window = InputWindow {
 			window,
 			surface,
-			cursor_position: None,
+			mouse_delta: None,
 			grabbed: true,
-			modifiers: ModifiersState::empty(),
+			modifiers: Modifiers::default(),
 			input_loop_tx,
 		};
 		input_window.set_grab(false);
 		input_window
 	}
 
-	pub fn handle_event(&mut self, event: Event<()>) {
+	pub fn handle_event(&mut self, event: Event<()>, elwt: &EventLoopWindowTarget<()>) {
 		match event {
-			Event::WindowEvent { event, .. } => self.handle_window_event(event),
-			Event::RedrawRequested(_) => self.redraw(),
-			_ => (),
+			Event::WindowEvent { window_id, event } if window_id == self.window.id() => match event
+			{
+				WindowEvent::CloseRequested => elwt.exit(),
+				_ => self.handle_window_event(event),
+			},
+			Event::DeviceEvent {
+				event: DeviceEvent::MouseMotion { delta },
+				..
+			} => {
+				self.handle_mouse_delta(delta);
+			}
+			// Doesn't seem to do anything, at least on wayland
+			// Event::DeviceEvent {
+			// 	event: DeviceEvent::MouseWheel { delta },
+			// 	..
+			// } => {
+			// 	dbg!(delta);
+			// 	match delta {
+			// 		MouseScrollDelta::LineDelta(x, y) => {
+			// 			send_input_ipc(Message::MouseAxisContinuous(vec2(x, y).into()))
+			// 		}
+			// 		MouseScrollDelta::PixelDelta(p) => send_input_ipc(Message::MouseAxisDiscrete(
+			// 			vec2(p.x as f32, p.y as f32).into(),
+			// 		)),
+			// 	}
+			// }
+			Event::AboutToWait => {
+				self.redraw();
+			}
+			_ => {}
 		}
+	}
+
+	fn handle_mouse_delta(&mut self, delta: (f64, f64)) {
+		if self.grabbed {
+			self.mouse_delta = Some(LogicalPosition::new(delta.0, delta.1));
+			send_input_ipc(Message::MouseMove([delta.0 as f32, delta.1 as f32].into()));
+		} else {
+			self.mouse_delta = None;
+		};
 	}
 
 	fn handle_window_event(&mut self, event: WindowEvent) {
 		match event {
 			WindowEvent::MouseInput { state, button, .. } => self.handle_mouse_input(state, button),
-			WindowEvent::CursorMoved { position, .. } => self.handle_mouse_move(position),
-			WindowEvent::KeyboardInput { input, .. } => self.handle_keyboard_input(input),
+			WindowEvent::MouseWheel { delta, .. } => match delta {
+				MouseScrollDelta::LineDelta(x, y) => {
+					send_input_ipc(Message::MouseAxisContinuous(vec2(x, y).into()))
+				}
+				MouseScrollDelta::PixelDelta(p) => send_input_ipc(Message::MouseAxisDiscrete(
+					vec2(p.x as f32, p.y as f32).into(),
+				)),
+			},
+			WindowEvent::KeyboardInput { event, .. } => self.handle_keyboard_input(event),
 			WindowEvent::ModifiersChanged(state) => self.modifiers = state,
-
 			WindowEvent::CursorEntered { .. } => {
 				send_input_ipc(Message::ResetInput);
 				self.input_loop_tx.send(StateChange::Enable).unwrap();
@@ -99,11 +145,16 @@ impl InputWindow {
 				send_input_ipc(Message::Disconnect);
 				exit(0);
 			}
+			WindowEvent::RedrawRequested => {
+				self.redraw();
+			}
 			_ => (),
 		}
 	}
 
 	fn redraw(&mut self) {
+		let delta = self.mouse_delta.unwrap_or_default();
+
 		let window_size = self.window.inner_size();
 		self.surface
 			.resize(
@@ -113,15 +164,12 @@ impl InputWindow {
 			.unwrap();
 		let mut buffer = self.surface.buffer_mut().unwrap();
 
-		let Some(mouse_position) = self.cursor_position else {
-			return;
-		};
+		let delta = vec2(delta.x as f32, delta.y as f32);
 		let window_center = vec2(
 			window_size.width as f32 / 2.0,
 			window_size.height as f32 / 2.0,
 		);
-		let mouse_position = vec2(mouse_position.x as f32, mouse_position.y as f32);
-		let delta = mouse_position - window_center;
+
 		let l1 = window_center;
 		let l2 = window_center + (delta * 4.0);
 		let thickness = 10.0;
@@ -139,42 +187,45 @@ impl InputWindow {
 		buffer.present().unwrap();
 	}
 
-	fn handle_mouse_move(&mut self, position: PhysicalPosition<f64>) {
-		self.cursor_position = if self.grabbed {
-			Some(position.to_logical::<u32>(self.window.scale_factor()))
-		} else {
-			None
-		};
-
-		if self.grabbed {
-			self.window.request_redraw();
-			let window_size = self.window.inner_size();
-			let center_position = LogicalPosition::new(
-				window_size.width as f64 / 2.0,
-				window_size.height as f64 / 2.0,
-			);
-			self.window.set_cursor_position(center_position).unwrap();
-		}
-	}
-
 	fn handle_mouse_input(&mut self, state: ElementState, button: MouseButton) {
 		if !self.grabbed {
 			if state == ElementState::Released && button == MouseButton::Left {
 				self.set_grab(true);
 			}
+			return;
 		}
+		let btn_id = match button {
+			MouseButton::Left => 272,
+			MouseButton::Right => 273,
+			MouseButton::Middle => todo!(),
+			MouseButton::Back => todo!(),
+			MouseButton::Forward => todo!(),
+			MouseButton::Other(n) => n,
+		};
+		send_input_ipc(Message::MouseButton {
+			button: btn_id as u32,
+			pressed: state == ElementState::Pressed,
+		})
 	}
 
-	fn handle_keyboard_input(&mut self, input: KeyboardInput) {
-		if input.virtual_keycode == Some(VirtualKeyCode::Z)
+	fn handle_keyboard_input(&mut self, input: KeyEvent) {
+		if input.logical_key.as_ref() == Key::Character("q")
 			&& input.state == ElementState::Released
-			&& self.modifiers.logo()
+			&& self.modifiers.state().super_key()
 		{
 			self.set_grab(false);
+			return;
 		}
+		let pressed = input.state == ElementState::Pressed;
+
+		let Some(keycode) = input.physical_key.to_scancode() else {
+			dbg!(":(");
+			return;
+		};
+		send_input_ipc(Message::Key { keycode, pressed });
 	}
 
-	const GRABBED_WINDOW_TITLE: &'static str = "Manifold Input (super+z to release cursor)";
+	const GRABBED_WINDOW_TITLE: &'static str = "Manifold Input (super+q to release cursor)";
 	const UNGRABBED_WINDOW_TITLE: &'static str = "Manifold Input (click to grab input)";
 	fn set_grab(&mut self, grab: bool) {
 		if grab == self.grabbed {
@@ -183,12 +234,7 @@ impl InputWindow {
 		self.grabbed = grab;
 
 		self.window.set_cursor_visible(!grab);
-		if grab {
-			let window_size = self.window.inner_size();
-			let center_position =
-				LogicalPosition::new(window_size.width / 2, window_size.height / 2);
-			self.window.set_cursor_position(center_position).unwrap();
-		}
+
 		let window_title = if grab {
 			Self::GRABBED_WINDOW_TITLE
 		} else {
@@ -200,7 +246,6 @@ impl InputWindow {
 		} else {
 			CursorGrabMode::None
 		};
-
 		if self.window.set_cursor_grab(grab).is_ok() {
 			self.window.set_title(window_title);
 		}
--- manifold/src/main.rs
+++ manifold/src/main.rs
@@ -1,22 +1,21 @@
 use input_window::InputWindow;
 use std::{io::IsTerminal, process::exit};
+use winit::event_loop::ControlFlow;
 use winit::{event_loop::EventLoopBuilder, platform::x11::EventLoopBuilderExtX11};
-
 pub mod input_window;
 
 fn main() {
-	if std::io::stdout().is_terminal() {
-		panic!("You need to pipe this into an input sink e.g. `manifold | azimuth`");
-	}
+	// if std::io::stdout().is_terminal() {
+	// 	panic!("You need to pipe this into an input sink e.g. `manifold | azimuth`");
+	// }
 	ctrlc::set_handler(|| exit(0)).unwrap();
-	let event_loop = EventLoopBuilder::new()
-		.with_any_thread(true)
-		.with_x11()
-		.build();
+	let event_loop = EventLoopBuilder::new().build().unwrap();
 	let mut input_window = InputWindow::new(&event_loop);
 
-	event_loop.run(move |event, _, control_flow| {
-		input_window.handle_event(event);
-		control_flow.set_wait();
-	});
+	event_loop
+		.run(move |event, elwt| {
+			elwt.set_control_flow(ControlFlow::Wait);
+			input_window.handle_event(event, elwt);
+		})
+		.unwrap();
 }
